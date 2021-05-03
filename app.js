e10ca6ac47ef7d0789320851bef7835e033f3f2f
abdenassar01 <abdenassaramimi99@gmail.com>
2021-05-04 01:54:10 +0200

    Uploading repesetory for first time

---
+++ app.js
@@ -0,0 +1,37 @@
+
+function changeToDark(){
+
+        document.querySelector('.continner').style.backgroundColor = "#1a2628";
+        document.querySelector('.imgCover').style.backgroundColor = "#102F2B";
+        document.querySelector('.me').style.backgroundColor = "#102F2B";
+};
+
+function changeToLight(){
+
+        document.querySelector('.continner').style.backgroundColor = "#15bae8";
+        document.querySelector('.imgCover').style.backgroundColor = "#14bb91";
+        document.querySelector('.me').style.backgroundColor = "#14bb91";
+};
+
+function cvDark(){
+
+        document.querySelector('.continner').style.backgroundColor = "#2D494A";
+        document.querySelector('.plangueges').style.backgroundColor = "#121212";
+        document.querySelector('.skills').style.backgroundColor = "#121212";
+        document.querySelector('.certificats').style.backgroundColor = "#121212";
+        document.querySelector('.plangueges').style.color = "#ADF1FA";
+        document.querySelector('.skills').style.color = "#ADF1FA";
+        document.querySelector('.certificats').style.color = "#ADF1FA";
+
+};
+
+function cvLight(){
+
+        document.querySelector('.continner').style.backgroundColor = "#15bae8";
+        document.querySelector('.plangueges').style.backgroundColor = "#ffffff";
+        document.querySelector('.skills').style.backgroundColor = "#ffffff";
+        document.querySelector('.certificats').style.backgroundColor = "#ffffff";
+        document.querySelector('.plangueges').style.color = "black";
+        document.querySelector('.skills').style.color = "black";
+        document.querySelector('.certificats').style.color = "black";
+};
